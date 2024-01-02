5015df8b90d4758bf1f88829df576b0ca9e79015
Linuxe <angelren1220@gmail.com>
2024-01-02 00:55:14 -0500

    add problem and steps

---
+++ CCC_21_S2.java
@@ -0,0 +1,15 @@
+/*
+ * 2021 Senior Problem 2
+ * Morden Art
+ */
+
+public class CCC_21_S2 {
+  
+  public static void main(String[] args) {
+    // 1. read the input
+
+    // 2. count the number of gold cells
+
+    // 3. output the result
+  }
+}
