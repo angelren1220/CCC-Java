dc09638077db940b6bdffde53df6e56484d5f192
Linuxe <angelren1220@gmail.com>
2024-01-02 01:09:05 -0500

    add steps

---
+++ CCC_21_S3.java
@@ -0,0 +1,15 @@
+/*
+ * 2021 Senior Problem 3
+ */
+
+public class CCC_21_S3 {
+
+  public static void main(String[] args) {
+    // 1. Read the number of friends N.
+    // 2. For each friend, read their initial position P_i, walking speed W_i, and hearing range D_i.
+    // 3. Determine the range of possible positions for c that would allow each friend to hear the concert.
+    // 4. For each possible position c, calculate the total time it would take all friends to reach a point within their hearing range.
+    // 5. Choose the position c that minimizes the total time.
+  }
+
+}
