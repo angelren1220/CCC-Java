5d8ec937c0bcf0542f89177e0a6271dfb1b8ca63
Linuxe <angelren1220@gmail.com>
2024-01-02 00:10:35 -0500

    add steps

---
+++ CCC_21_S1.java
@@ -0,0 +1,24 @@
+/*
+ * 2021 Senior Problem 1 Crazy Fencing
+ */
+
+public class CCC_21_S1 {
+
+// 1. Read Input:
+
+// Use BufferedReader to read the input.
+// Read the first line to get the number of pieces of wood, N.
+// Read the second line to get the heights of the pieces of wood into an array heights, which will have N+1 elements.
+// Read the third line to get the widths of the pieces of wood into an array widths, which will have N elements.
+// 2. Calculate Area of Each Piece:
+
+// Iterate through each piece of wood.
+// Calculate the area of each trapezoid using the formula area = (a + b) / 2 * h, where a and b are the heights of the left and right sides of a piece of wood, and h is the width.
+// 3. Sum Areas:
+
+// Accumulate the areas of all the pieces to get the total area.
+// 4. Output the Result:
+
+// Print out the total area.
+
+}
